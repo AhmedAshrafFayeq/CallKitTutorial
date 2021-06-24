620ff0e0f970afc856acb636b531b12672e6d00b
Ahmed Fayek <ahmed.fayek@gizasystems.com>
2021-06-24 15:45:00 +0200

    customize callKit change name and ImageIcon

--- CallKitTutorial/ViewController.swift
+++ CallKitTutorial/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController,CXProviderDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        sendACall()
+          
+        //recieveACall()
+        //sendACall()
+//        connectACall()
+        
+        customizeCallKit()
         
     }
     
@@ -48,6 +52,32 @@ class ViewController: UIViewController,CXProviderDelegate {
                 let controller = CXCallController()
                 let transaction = CXTransaction(action: CXStartCallAction(call: UUID(), handle: CXHandle(type: .generic, value: "FAYEQ")))
                 controller.request(transaction, completion: { error in })
+    }
+    
+    func connectACall(){
+        let provider = CXProvider(configuration: CXProviderConfiguration())
+                provider.setDelegate(self, queue: nil)
+                let controller = CXCallController()
+                let transaction = CXTransaction(action: CXStartCallAction(call: UUID(), handle: CXHandle(type: .generic, value: "FAYEQ")))
+                controller.request(transaction, completion: { error in })
+
+                DispatchQueue.main.asyncAfter(wallDeadline: DispatchWallTime.now() + 5) {
+                    provider.reportOutgoingCall(with: controller.callObserver.calls[0].uuid, connectedAt: nil)
+                }
+    }
+    
+    func customizeCallKit(){
+        let config = CXProviderConfiguration()
+        config.iconTemplateImageData = UIImage(named: "fayeqImg")!.pngData()
+                config.ringtoneSound = "ringtone.caf"
+                config.includesCallsInRecents = false;
+                config.supportsVideo = true;
+                let provider = CXProvider(configuration: config)
+                provider.setDelegate(self, queue: nil)
+                let update = CXCallUpdate()
+                update.remoteHandle = CXHandle(type: .generic, value: "FAYEQ")
+                update.hasVideo = true
+                provider.reportNewIncomingCall(with: UUID(), update: update, completion: { error in })
     }
 
 }
