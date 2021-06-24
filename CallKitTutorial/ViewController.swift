5b1bdf59d91442f93b2c858d0b1b6e3a9347c741
Ahmed Fayek <ahmed.fayek@gizasystems.com>
2021-06-24 15:30:12 +0200

    recieve a call done

--- CallKitTutorial/ViewController.swift
+++ CallKitTutorial/ViewController.swift
@@ -6,13 +6,33 @@
 //
 
 import UIKit
+import CallKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CXProviderDelegate {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let provider = CXProvider(configuration:
+                                    CXProviderConfiguration())
+                provider.setDelegate(self, queue: nil)
+                let update = CXCallUpdate()
+                update.remoteHandle = CXHandle(type: .generic, value: "Fayeq")
+                provider.reportNewIncomingCall(with: UUID(), update: update, completion: { error in })
     }
+    
+    func providerDidReset(_ provider: CXProvider) {
+        
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
+        action.fulfill()
+    }
+
+    func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+        action.fulfill()
+    }
+
 
 
 }
